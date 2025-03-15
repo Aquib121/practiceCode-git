e2cb105172e60b7231ff73291215b58ea578bc34
my_style <shamimaquib12@gmail.com>
2025-03-16 03:46:23 +0530

    binary search added

---
+++ binarySearch.py
@@ -0,0 +1,20 @@
+def main(n,q):
+    final = binarySearch(n,q)
+    print(final)
+
+def binarySearch(nums,target):
+    l = 0
+    r = len(nums)-1
+    while l <= r:
+        mid = (l+r)//2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            r = mid - 1
+        else:
+            l = mid + 1
+    return -1
+
+nums = [1,4,5,7,8,9,12,34,65]
+target = 9
+main(nums, target)
